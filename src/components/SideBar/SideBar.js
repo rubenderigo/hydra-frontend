02d902a30382289a9ccf9d9ae8aafcc713dba23b
rubenderigo <rm.derigo@gmail.com>
2022-01-04 11:38:33 -0300

    fix position button

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,18 +1,21 @@
+import { useAuth } from 'context/UserContext';
 import { Link } from 'react-router-dom';
 
 import styles from './SideBar.module.css';
 
 const SideBar = () => {
+  const { user } = useAuth();
+  
   return (
     <div className={styles['side-bar']}>
       <div className={styles['header']}>
         <div className={styles['presantation']}>
           <p className={styles['hello']}>Hola, </p>
-          <p className={styles['username']}>nombreUsuario</p>
+          <p className={styles['username']}>{user.username}</p>
         </div>
         <div className={styles['img-user']}>
           <img
-            src="https://i.pinimg.com/564x/ae/09/14/ae0914d5c9e3b0499ed42fea4aa0c987.jpg"
+            src={user.image}
             alt="profile"
           />
         </div>
@@ -22,19 +25,19 @@ const SideBar = () => {
           <nav>
             <ul>
               <li>
-                <span class="material-icons">home</span>
+                <span className="material-icons">home</span>
                 Inicio
               </li>
               <li>
-                <span class="material-icons">account_circle</span>
+                <span className="material-icons">account_circle</span>
                 Mi perfil
               </li>
               <li>
-                <span class="material-icons">rate_review</span>
+                <span className="material-icons">rate_review</span>
                 Mis reportes
               </li>
               <li>
-                <span class="material-icons">settings</span>
+                <span className="material-icons">settings</span>
                 Configuraciones
               </li>
             </ul>
@@ -42,7 +45,7 @@ const SideBar = () => {
         </div>
         <div className={styles['link']}>
           <Link to="/" className={styles['sing-out-link']}>
-            <span class="material-icons">logout</span>
+            <span className="material-icons">logout</span>
             cerrar sesión
           </Link>
         </div>
